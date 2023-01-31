b67e97de291ba4a853d87c888d7f2ace512efd6a
CyberRock66 <vasylhryniuk6@gmail.com>
2023-01-31 23:29:57 +0200

    [added] active link for header nav

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,44 +1,68 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-export const Header = () => (
-  <header>
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <a className="navbar-brand" href="index.html">
-          conduit
-        </a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <a className="nav-link active" href="/">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">
-              {' '}
-              <i className="ion-compose" />
-              &nbsp;New Article{' '}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">
-              {' '}
-              <i className="ion-gear-a" />
-              &nbsp;Settings{' '}
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/auth/login">
-              Sign in
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/auth/register">
-              Sign up
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+export const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            conduit
+          </a>
+          <ul className="nav navbar-nav pull-xs-right">
+            <li className="nav-item">
+              <Link
+                className={
+                  router.pathname === '/' ? 'nav-link active' : 'nav-link'
+                }
+                href="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                {' '}
+                <i className="ion-compose" />
+                &nbsp;New Article{' '}
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                {' '}
+                <i className="ion-gear-a" />
+                &nbsp;Settings{' '}
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={
+                  router.pathname === '/auth/login'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+                href="/auth/login"
+              >
+                Sign in
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={
+                  router.pathname === '/auth/register'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+                href="/auth/register"
+              >
+                Sign up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
